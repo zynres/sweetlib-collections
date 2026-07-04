63493b61d20c4e5d9e2da2a899d61213bfb05545
Zynres <killp8918@gmail.com>
2026-07-05 02:09:36 +0300

    feat: init the field of collection

--- src/hash_set/UnsafeHashSet.cs
+++ src/hash_set/UnsafeHashSet.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace unsafe_maps.src.hash_set;
 
 public unsafe struct UnsafeHashSet<T> : IDisposable where T : unmanaged
@@ -10,4 +12,18 @@ public unsafe struct UnsafeHashSet<T> : IDisposable where T : unmanaged
 
     public uint Lenght;
     public uint Capacity;
+
+    public UnsafeHashSet(uint capacity, uint division = 2)
+    {
+        this.division = division;
+
+        Capacity = capacity;
+        bucketCapacity = capacity / division;
+
+        Slot = (Slot<T>*)NativeMemory.Alloc((nuint)(sizeof(Slot<T>) * capacity));
+        Bucket = (uint?*)NativeMemory.Alloc(sizeof(uint) * bucketCapacity);
+
+        NativeMemory.Clear(Slot, (nuint)sizeof(Slot<T>) * capacity);
+        NativeMemory.Clear(Bucket, sizeof(int) * bucketCapacity);
+    }
 }
